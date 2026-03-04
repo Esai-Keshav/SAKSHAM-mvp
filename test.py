be82fdec40c3b1f04a7e5f545fbe1f22de62d222
Esai Keshav <magicesaik7@gmail.com>
2026-03-04 12:16:34 +0530

    Added auto commit

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("hello")
+hook = "https://api.render.com/deploy/srv-d6jcadbuibrs73aifsdg?key=hGMzyQEAPv4"
+
+import urllib3
+
+
+def trigger_deploy():
+    http = urllib3.PoolManager()
+    try:
+        response = http.request("GET", hook, timeout=10.0)
+        return response.status
+    except Exception as e:
+        print(f"Error: {e}")
+        return 500
+
+
+if __name__ == "__main__":
+    print(trigger_deploy())
